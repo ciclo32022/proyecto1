c035f5ce57a82914b8b92f58398f29451a4acf75
Daniel Carmona <carmonadacg@hotmail.com>
2022-09-13 17:23:13 -0500

    13 sept

--- Proyecto/parkapp/src/main/java/co/parkapp/parkapp/controllers/visitacontroller.java
+++ Proyecto/parkapp/src/main/java/co/parkapp/parkapp/controllers/visitacontroller.java
@@ -1,16 +1,10 @@
 package co.parkapp.parkapp.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import co.parkapp.parkapp.models.dao.visitasdao;
 
 @Controller
 public class visitacontroller {
 
-	@Autowired
-	private visitasdao visitadao;
-
+	
 	
 }
